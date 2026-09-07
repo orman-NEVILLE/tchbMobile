8a58aa08ad7b695954735e3768e14e84858b3614
orman-NEVILLE <21mk386@esisalama.org>
2026-09-07 17:31:36 +0200

    Configuration du plugin PWA

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'TCHB Mobile',
+        short_name: 'TCHB',
+        description: 'Application mobile TCHB',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
